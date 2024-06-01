6680cd4976f7a3564b871c7de08ffff73c474083
sk5614 <sk5614@kakao.com>
2024-06-01 23:51:26 +0900

    Initial commit

---
+++ src/test/java/controller/BoardController.java
@@ -0,0 +1,14 @@
+package controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BoardController {
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String index() {
+		System.out.println("인덱스 페이지 호출");
+		return "index";
+	}
+
+}
