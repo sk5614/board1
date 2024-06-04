006337c55ac98a354940d7f44447159d4532d435
sk5614 <sk5614@kakao.com>
2024-06-04 23:02:22 +0900

    0604

---
+++ src/test/java/com/boot/board/domain/Board.java
@@ -0,0 +1,16 @@
+package com.boot.board.domain;
+
+import lombok.Data;
+
+@Data
+public class Board {
+	
+	
+	private int b_id;
+	private String b_title;
+	private String b_writer;
+	private String b_contend;
+	private String b_date;
+	
+	
+}
